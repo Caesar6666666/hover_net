e6a5bdb1da9fa25ecd52763c0cb5cec62b3cf99a
liusunyan <l@bupt.edu.cn>
2024-04-17 07:55:14 +0000

    推理脚本修改

--- run_tile.sh
+++ run_tile.sh
@@ -1,14 +1,14 @@
 python run_infer.py \
---gpu='1' \
---nr_types=6 \
+--gpu='0,1,2,3,4,5' \
+--nr_types=5 \
 --type_info_path=type_info.json \
 --batch_size=64 \
---model_mode=fast \
---model_path=../pretrained/hovernet_fast_pannuke_type_tf2pytorch.tar \
+--model_mode=original \
+--model_path=hovernet_original_consep_type_tf2pytorch.tar \
 --nr_inference_workers=8 \
 --nr_post_proc_workers=16 \
 tile \
---input_dir=dataset/sample_tiles/imgs/ \
+--input_dir=/home/liusunyan/hover_net/pre_process/tiles \
 --output_dir=dataset/sample_tiles/pred/ \
 --mem_usage=0.1 \
 --draw_dot \
